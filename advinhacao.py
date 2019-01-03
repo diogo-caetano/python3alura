3c725e9cd9de3e105b3d853ec48a4368ec4d9458
diogo-caetano <matrixeoMelhorfilme>
2019-01-03 16:46:42 -0200

    aula 4 finalizada

--- advinhacao.py
+++ advinhacao.py
@@ -3,24 +3,26 @@ print("Bem vindo no jogo de advinhação!")
 print("********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número:")
+while (rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número:")
+    print("Você digitou" , chute_str)
+    chute = int(chute_str)
 
-print("Você digitou" , chute_str)
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-chute = int(chute_str)
+    if (acertou):
+        print("Você acertou")
+    else:
+        if (maior):
+            print("Você errou, o seu chute foi maior do que o número secreto.")
+        elif (menor):
+            print("Você errou, o seu chute foi menor do que o número secreto.")
+        rodada = rodada + 1
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
-
-if (acertou):
-    print("Você acertou")
-else:
-    if (maior):
-        print("Você errou, o seu chute foi maior do que o número secreto.")
-    elif (menor):
-        print("Você errou, o seu chute foi menor do que o número secreto.")
-
-
-print("Fim do jogo!")       
+    print("Fim do jogo!")       
