1036893986288499f10c61c0c5221598e71c4cfa
Diogo <diogo_caetano@live.com>
2019-01-22 22:04:20 -0200

    exercicio 4 do site pythonexercises

--- python3praticepython/exercise 3.py
+++ python3praticepython/exercise 3.py
@@ -1,5 +1,23 @@
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#
+# for element in a:
+#     if(element < 5):
+#         print(element)
+#  exercício feito
+
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#
+# for element in a:
+#     if(element < 5):
+#         new_list = element
+#         print(new_list)
+#desafio 1 completo
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
-for element in a:
-    if(element < 5):
-        print(element)
+numero_escolhido = int(input("Qual o número?"))
+
+for numero in a:
+    if numero <= numero_escolhido:
+        print(numero)
+
