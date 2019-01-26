4c5aaace0eb1255899c7ec03fc22f4598f341b80
diogo-caetano <matrixeoMelhorfilme>
2019-01-26 15:37:26 -0200

    estudando no trabalho , centro

--- python3aluracurso3/conta.py
+++ python3aluracurso3/conta.py
@@ -1,2 +1,12 @@
 class Conta:
-    pass
+    def __init__(self, numero, titular, saldo, limite):
+        print ("Construindo objeto ... {}".format(self))
+        self.numero     = numero
+        self.titular    = titular
+        self.saldo      = saldo
+        self.limite     = limite
+
+
+from conta import Conta
+conta = Conta(123, "Diogo", 100.0, 1000.0)
+print(conta.titular)
