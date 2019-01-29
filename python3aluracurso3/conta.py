8a8d01f22bf7fac186661740a908b431995d3514
Diogo <diogo_caetano@live.com>
2019-01-29 21:28:41 -0200

    finalizando aula 03 com exercício de datas formatadas.

--- python3aluracurso3/conta.py
+++ python3aluracurso3/conta.py
@@ -6,7 +6,25 @@ class Conta:
         self.saldo      = saldo
         self.limite     = limite
 
+    def extrato(self):
+        print("O saldo do {} é de {} ".format(self.titular, self.saldo))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
+
 
 from conta import Conta
-conta = Conta(123, "Diogo", 100.0, 1000.0)
-print(conta.titular)
+
+input("Qual conta quer ver ?")
+
+if input("1"):
+    conta = Conta(123, "Diogo", 100.0, 1000.0)
+    print(conta.titular)
+    conta.extrato()
+elif("2"):
+    conta2 = Conta(321, "Maria", 1000.0, 10000.0)
+    print(conta.titular)
+    conta.extrato()
