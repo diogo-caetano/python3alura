c28f9c12c760bb09753dce2e035368c4b2d0407f
diogo-caetano <matrixeoMelhorfilme>
2019-02-05 19:42:53 -0200

    Final do curso de python 3 da alura. Final da alura.

--- python3aluracurso3/conta.py
+++ python3aluracurso3/conta.py
@@ -1,10 +1,10 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print ("Construindo objeto ... {}".format(self))
-        self.__numero     = numero
-        self.__titular    = titular
-        self.__saldo      = saldo
-        self.__limite     = limite
+        self.__numero       = numero
+        self.__titular      = titular
+        self.__saldo        = saldo
+        self.__limite       = limite
 
     def extrato(self):
         print("O saldo do {} é de {} ".format(self.__titular, self.__saldo))
@@ -12,8 +12,16 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel
+
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else
+            print("O valor {} passou o limite.".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
@@ -35,6 +43,13 @@ class Conta:
     def limite(self, limite):
         self.__limite = limite
 
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco': '237'}
 
 #
 # from conta import Conta
